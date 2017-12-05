609c4cf6d030fd25ce8d78360a3cde31b9fc2661
Seiji Matsuoka <mojaie@aol.com>
2017-12-05 15:03:05 +0900

    v0.7.4

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="Chorus",
-    version="0.7.3",
+    version="0.7.4",
     description="Simple chemical structure modeling toolkit",
     long_description=long_description,
     url="https://github.com/mojaie/chorus",
