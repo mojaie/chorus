fe33659e8df751dfcbef86eee0b6258ce8090d5f
Seiji Matsuoka <mojaie@aol.com>
2017-12-20 11:24:57 +0900

    Cython setting for Mac OS X

--- chorus/cython/cython_setup.py
+++ chorus/cython/cython_setup.py
@@ -1,3 +1,6 @@
+
+import sys
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
@@ -5,13 +8,15 @@ from Cython.Compiler import Options
 
 Options.annotate = True
 
-setup(
-    cmdclass={'build_ext': build_ext},
-    ext_modules=[
-        Extension(
-            "mcsdr", sources=["mcsdr.pyx"], language="c++",
-            extra_compile_args=["-std=c++11", "-stdlib=libc++"],
-            extra_link_args=["-std=c++11", "-stdlib=libc++"]
-        )
-    ]
-)
+if sys.platform.startswith("darwin"):  # clang (Mac OS X)
+    ext = Extension(
+        "mcsdr", sources=["mcsdr.pyx"], language="c++",
+        extra_compile_args=["-std=c++11", "-stdlib=libc++"],
+        extra_link_args=["-std=c++11", "-stdlib=libc++"]
+    )
+else:  # gcc
+    ext = Extension(
+        "mcsdr", sources=["mcsdr.pyx"], language="c++"
+    )
+
+setup(cmdclass={'build_ext': build_ext}, ext_modules=[ext])
